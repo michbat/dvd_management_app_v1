60e059249257beceb5b58437e83b170a151aa819
michbat <michael.batn@hotmail.com>
2025-09-21 01:10:01 +0200

    testing show_table method in main()

--- main.py
+++ main.py
@@ -8,8 +8,17 @@ from dvd_manager import DVDManager
 
 def main():
     dvd_manager: DVDManager = DVDManager()
-    message: str = dvd_manager.create_table()
-    print(message)
+    # message: str = dvd_manager.create_table()
+    # print(message)
+    # print()
+
+    # Afficher la liste des DVD dans la BDD
+    try:
+        dvd_manager.dvd_list = dvd_manager.show_table()
+        for item in dvd_manager.dvd_list:
+            print(item)
+    except ValueError as e:
+        print(e)
 
 
 if __name__ == "__main__":
